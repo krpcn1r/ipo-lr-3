e151170aa8af162ca55a7ee0d52cad5777804c58
krpcn1r <zaystep09@gmail.com>
2025-10-02 17:14:56 +0300

    Create main.py

---
+++ task_3/main.py
@@ -0,0 +1,13 @@
+day = int(input("Введите день: ")) #  Заправшиваем день
+months = int(input("Введите месяц: ")) # Запрашиваем месяц
+
+if months >= 3 and months <= 5 and day >= 1 and day <= 31: # Проверяем число и дату на весну
+    print("Весна") 
+elif months >= 6 and months <= 8 and day >= 1 and day <= 31: # Проверяем число и дату на лето
+    print("Лето")
+elif months >= 9 and months <= 11 and day >= 1 and day <= 30: # Проверяем число и дату на осень
+    print("Осень")
+elif months >= 12 and months <= 2 and day >= 1 and day <= 28: # Проверяем число и дату на зиму
+    print("Зима")
+else:
+    print("Нет такой даты") # Если введенеа неверная дата
